91ea9a78746bd22af669868686a620654c75e216
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-15 21:23:46 -0700

    added city card and footer

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export default class Footer extends Component{
     render(){
         return(
-            <div>
-                
-            </div>
+            <footer class="footer">
+                <div class="container">
+                    <span class="text-muted">Copyright (C) 2018, Niti Singhal.</span>
+                </div>
+            </footer>
         )
     }
 }
