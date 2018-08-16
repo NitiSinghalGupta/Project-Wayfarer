9af04a3e43922e53c7ae61015e2882a11236de3b
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-15 21:19:19 -0700

    added city card

--- client/src/component/City.js
+++ client/src/component/City.js
@@ -3,8 +3,10 @@ import React,{Component} from 'react';
 export default class City extends Component {
     render(){
         return(
-            <div>
-                
+            <div class="card">
+            <div class="card-body">
+                CITY NAME
+            </div>
             </div>
         )
     }
