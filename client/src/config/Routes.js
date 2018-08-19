daedaf1616628ef03e83f916c0c71f71b7dff4f6
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-19 09:56:00 -0700

    import profile

--- client/src/config/Routes.js
+++ client/src/config/Routes.js
@@ -2,7 +2,7 @@ import React, { Component }from 'react';
 import {Switch, Route} from 'react-router-dom';
 import  HomePage from '../View/HomePage';
 import  Landing from '../View/Landing';
-
+import  Profile from '../View/Profile';
 
 export default class Routes extends Component{
 
