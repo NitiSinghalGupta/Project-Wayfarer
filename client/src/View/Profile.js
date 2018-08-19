9fd18565084d0df7c9cf66873c3ca1c271ac236c
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-19 09:54:22 -0700

    added profile page layout html

--- client/src/View/Profile.js
+++ client/src/View/Profile.js
@@ -4,9 +4,44 @@ import React,{Component} from 'react';
 export default class Profile extends Component {
     render(){
         return(
-            <div>
-                
+          
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-4 col-xs-offset-4">
+
+                        <img class="img-responsive picture" src="http://www.kusalimika.ch/file/2017/02/avaar.jpg" />
+                        <div>
+                            <h5 class="text-bold"></h5>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4 col-xs-offset-4 text-left">
+                        <div class=" col-xs-6 tital">First Name:</div>
+                        <div class=" col-xs-6 ">Marko</div>
+                        <div class="clearfix"></div>
+                        <div class="bot-border"></div>
+
+                        <div class="col-xs-6 tital ">Last Name:</div>
+                        <div class="col-xs-6"> Markovic</div>
+                        <div class="clearfix"></div>
+                        <div class="bot-border"></div>
+
+                        <div class="col-xs-6 tital ">Age:</div>
+                        <div class="col-xs-6"> 25</div>
+                        <div class="clearfix"></div>
+                        <div class="bot-border"></div>
+
+                        <div class="col-xs-6 tital ">Place Of Birth:</div>
+                        <div class="col-xs-6">Doboj</div>
+                        <div class="clearfix"></div>
+                        <div class="bot-border"></div>
+                    </div>
+                </div>    
             </div>
+
+
         )
     }
 }
