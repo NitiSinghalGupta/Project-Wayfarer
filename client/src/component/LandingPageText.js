67570a4d69259dea59c671b613d5d2223c992613
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-18 21:41:48 -0700

    added test images to landing text card

--- client/src/component/LandingPageText.js
+++ client/src/component/LandingPageText.js
@@ -5,19 +5,19 @@ export default class LandingPageText extends Component{
         return(
             <div className="landingpagetext">
                 <div class=" cardone card" style={{width: '18rem'}}>
-                    <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap" />
+                    <img class="card-img-top" src="https://az760333.vo.msecnd.net/-/media/corporate/footer-section/shutterstock_210836533.jpg?la=en&ts=6e058b19-5741-4545-a575-5d4016a04962" alt="Card image cap" />
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
                 <div class=" cardtwo card" style={{width: '18rem'}}>
-                    <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap" />
+                    <img class="card-img-top" src="https://az760333.vo.msecnd.net/-/media/corporate/footer-section/shutterstock_210836533.jpg?la=en&ts=6e058b19-5741-4545-a575-5d4016a04962" alt="Card image cap" />
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
                 <div class=" cardthree card" style={{width: '18rem'}}>
-                    <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap" />
+                    <img class="card-img-top" src="https://az760333.vo.msecnd.net/-/media/corporate/footer-section/shutterstock_210836533.jpg?la=en&ts=6e058b19-5741-4545-a575-5d4016a04962" alt="Card image cap" />
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
