6214ee3d8b3ad257e9f761805f05fb23cccf1fa9
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-08-21 13:42:37 -0700

    aligned post image to center

--- client/src/component/Post.js
+++ client/src/component/Post.js
@@ -24,11 +24,20 @@ export default class Post extends Component {
     render() {
         return <Group>
             <div className='row'>
-                <div className='col-md-10'>
+            <div className='col-md-6 postrow'>
+                <div className=" text-center postimage">
+                    <img className="img-fluid mx-auto d-block rounded city-image" src="https://az760333.vo.msecnd.net/-/media/corporate/footer-section/shutterstock_210836533.jpg?la=en&ts=6e058b19-5741-4545-a575-5d4016a04962" /> 
+                </div>
+                
+                <div className='col-md-6 postheader'>
+                
                     <div>{ this.props.post.title }</div>
+                    <br/>
                     <div>written by { this.props.post.email }</div>
                 </div>
-                <div className='col-md-2'>
+                </div>
+   
+                <div className='col-md-2 postbuttons'>
                     <IfClause condition={ this.props.user.email == this.props.post.email }>
                         <a href='#' onClick={ () => this.editPostClickHandler() }>Edit</a> | <a href='#' onClick={ () => this.deleteClickHandler() } >Delete</a>
                     </IfClause>
